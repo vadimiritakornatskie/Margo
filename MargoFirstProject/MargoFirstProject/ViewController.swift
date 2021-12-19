a284eaaa1fb9946d961b3af011e0137fb1ce355b
Kornackie <mloojuym@gmail.com>
2021-12-19 20:43:07 +0400

    initial commit

---
+++ MargoFirstProject/MargoFirstProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MargoFirstProject
+//
+//  Created by Вадим Корнацкий on 19.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+// Здарова Вадим
